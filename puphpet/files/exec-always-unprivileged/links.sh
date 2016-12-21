62c2a0173e5ce0e2757b9f90b1fe8252af7d09ac
Andrew Shell <andrew@andrewshell.org>
2016-12-21 11:17:14 -0600

    Fixing paths and permission issues

--- puphpet/files/exec-always-unprivileged/links.sh
+++ puphpet/files/exec-always-unprivileged/links.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 cd /var/www/pinpoint.dev/
 cp config/phinx.vagrant.php config/phinx.php
-composer install
+composer -qn install
+npm --loglevel=warn install
 bower install
 gulp less
 vendor/bin/phinx migrate
