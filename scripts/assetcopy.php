4f03f08d13052f7fded9829fc4d1bfdb4e45c327
Andrew Shell <andrew@andrewshell.org>
2018-01-31 10:08:52 -0600

    Minor fixes for new version

--- scripts/assetcopy.php
+++
@@ -1,9 +0,0 @@
-<?php
-$copyFolders = [
-    __DIR__ . '/../vendor/maximebf/debugbar/src/DebugBar/Resources' =>
-        __DIR__ . '/../public/debugbar'
-];
-
-foreach ($copyFolders as $from => $to) {
-    passthru(sprintf('cp -r %s %s', escapeshellarg($from), escapeshellarg($to)));
-}
